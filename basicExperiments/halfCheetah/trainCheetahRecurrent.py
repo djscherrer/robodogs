b1bbbd7aa36335e44a052f9684dd2afd11431437
Constantin Pinkl <tino122001@gmail.com>
2025-11-25 10:36:06 +0100

    Enabling Tracking Again

--- basicExperiments/halfCheetah/trainCheetahRecurrent.py
+++ basicExperiments/halfCheetah/trainCheetahRecurrent.py
@@ -28,7 +28,7 @@ class Args:
     """if toggled, `torch.backends.cudnn.deterministic=False`"""
     cuda: bool = False
     """if toggled, cuda will be enabled by default"""
-    track: bool = False
+    track: bool = True
     """if toggled, this experiment will be tracked with Weights and Biases"""
     wandb_project_name: str = "robodogs-cheetah"
     """the wandb's project name"""
